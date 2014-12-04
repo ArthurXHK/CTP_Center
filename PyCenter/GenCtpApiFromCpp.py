4b579765e3b3cea7b7e1b8cf67f5b8a7368a7045
jebin <cepinlin@gmail.com>
2014-12-04 20:42:33 +0800

    fix pattern regex

--- PyCenter/GenCtpApiFromCpp.py
+++ PyCenter/GenCtpApiFromCpp.py
@@ -4,29 +4,26 @@ f = open("..\\include\\ThostFtdcUserApiDataType.h")
 
 out = open(".\\CTPApiDataType.py", 'a')
 line = f.readline()
-charnumPattern = re.compile('[1-9]+')
-typedefPattern = re.compile('([a-z]|[A-Z])+')
+
 out.write('from ctypes import *\n')
 while line:
     
+    defineGroup = re.search('#define( +|\t+)(\w+)( +|\t+)((\'|\d|\w|#)+)', line)
     
-    if len(line) > 8:
-        if line[0] != '/':
-            if line[0:7] == '#define':
-                line = re.split(' *', line)
-                if len(line) == 3:
-                    out.write(line[1] + ' = ' + line[2])
-                    
-            elif line[0:7] == 'typedef':
-                line = re.split(' *', line)
-                typestring = line[1]
-                typename = typedefPattern.search(line[2])
-                charnum = charnumPattern.search(line[2])
-                print typename.group()
-                resstring = typename.group() + ' =' + ' c_' + typestring
-                if charnum != None:
-                    resstring = resstring + '*' + charnum.group()
-                out.write(resstring + '\n')
+    if defineGroup != None:
+        print defineGroup.group()
+        resstring = defineGroup.group(2) + ' = ' + defineGroup.group(4) + '\n'
+        out.write(resstring)
+    
+    typedefGroup = re.search('typedef( +|\t+)(\w+)( +|\t+)(\w+)\[*(\d*)\]*', line)
+    if typedefGroup != None:
+        resstring = typedefGroup.group(4) + ' = c_' + typedefGroup.group(2)
+        print typedefGroup.group()
+        if len(typedefGroup.group(5)):
+            print typedefGroup.group(5)
+            resstring += ' * ' + typedefGroup.group(5)
+            
+        out.write(resstring + '\n')
     line = f.readline()
     
 f.close()
@@ -56,3 +53,5 @@ while line:
             isOverGroup = re.search('}', line);
         out.write(' '*11 + ']\n')
     line = f.readline()
+f.close()
+out.close()
